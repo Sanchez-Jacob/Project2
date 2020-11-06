1b7b76e1190524d70ff30fc3bc196e3d5a822436
ElGueroHouston <sanchezjacob55@gmail.com>
2020-11-06 14:45:01 -0600

    Code changes

---
+++ project 2/Assets/Rocket.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour{
+    Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start(){
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        ProcessInput();
+        
+    }
+
+    private void ProcessInput(){
+        //GetKey applies all the same time and will report the status of the named key
+        if (Input.GetKey(KeyCode.Space)){
+            print("space pressed");
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+        //rotates to the left
+        if(Input.GetKey(KeyCode.A)){
+            print("turning left");
+            rigidBody.AddRelativeForce(Vector3.left);
+            
+        }
+        //rotates to the right
+        else if(Input.GetKey(KeyCode.D)){
+            print("turning right");
+            rigidBody.AddRelativeForce(Vector3.right);
+        }
+        
+            
+             
+
+    }
+}
